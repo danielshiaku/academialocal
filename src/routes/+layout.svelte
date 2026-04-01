a1f14b7a88b165abe82a952d13223e4739afc95d
Daniel Daki <daniel_daki@clio.nied.unicamp.br>
2026-04-01 11:51:48 -0300

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+	import favicon from '$lib/assets/favicon.svg';
+	import '../global.css';
+	import '@fontsource-variable/cabin/wght.css';
+	import '@fontsource-variable/roboto-condensed/wght.css';
+
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+{@render children()}
+
