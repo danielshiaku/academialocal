cce29f776151e9713d7deb57ee3f4cf51ba2de87
Daniel Shiaku <danielshiaku7@gmail.com>
2026-04-28 10:03:42 -0300

    graph

--- src/lib/SeoHome.svelte
+++ src/lib/SeoHome.svelte
@@ -3,10 +3,15 @@
 
     let site = "Academia Local"
     let separator = " - "
+    import img from "$lib/assets/musculacao.avif"
 </script>
 
 <svelte:head>
     <title>{site}{separator}A academia pertinho de você</title>
     <meta name={name} content={description}>
+
+    <meta property="og: title" content="Academia Local">
+    <meta property="og: description" content={description}>
+    <meta property="og: image" content={img}>
 </svelte:head>
     
