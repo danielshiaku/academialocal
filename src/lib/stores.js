1a1b3a0619cddb1fcb508592b17d89e5aafa9552
Daniel Shiaku <danielshiaku7@gmail.com>
2026-04-28 09:36:03 -0300

    correção de erros

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -3,26 +3,26 @@ import { writable } from "svelte/store";
 export const modalidades = writable([
     {name: 'Musculação',
     slug: 'musculação',
-    img: '/musculacao.jpg',
+    img: '/musculacao.avif',
     content: 'treinos de musculação individual ou em grupo'},
 
     {name: 'Crossfit',
     slug: 'crossfit',
-    img: '/crossfit.jpg',
+    img: '/crossfit.avif',
     content: 'treinos de musculação individual ou em grupo'},
 
     {name: 'Alongamento',
     slug: 'alongamento',
-    img: '/alongamento.jpg',
+    img: '/alongamento.avif',
     content: 'treinos de musculação individual ou em grupo'},
 
     {name: 'Exercícios em grupo',
     slug: 'exercicios',
-    img: '/exercicios-grupo.jpg',
+    img: '/exercicios-grupo.avif',
     content: 'treinos de musculação individual ou em grupo'},
 
     {name: 'Natação',
     slug: 'natacao',
-    img: '/natacao.jpg',
+    img: '/natacao.avif',
     content: 'treinos de musculação individual ou em grupo'}
 ])
